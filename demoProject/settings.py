6441d9415c9ba2bc5d4262af3c3c71b3603709cb
kishoor <kishoor0920@gmail.com>
2024-10-16 00:48:20 +0530

    allowed hosts added

--- demoProject/settings.py
+++ demoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8))b#5n!%h9hv1v!&==tyut*vos%b%u)m%^ttrhfjbj4=t2y%0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.verce.app']
 
 
 # Application definition
